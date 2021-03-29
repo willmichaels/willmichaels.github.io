e68cac1981fea349fe4e1ed4e225b0913823a269
willmichaels <68669735+willmichaels@users.noreply.github.com>
2021-03-29 18:06:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("coming soon");
+var ctx = document.getElementById('myChart');
+
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
+
+var myChart = new Chart(ctx, {
+ type: 'line',
+ data: {
+    labels: frameworks,
+    datasets: [{
+        label: 'Github Stars',
+        data: stars
+    }]
+ },
+})
